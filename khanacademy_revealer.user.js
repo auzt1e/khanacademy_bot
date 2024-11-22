1bae77f8c6040afac1322a8c2d4cf44c09263cf1
auzt1e <61610901+auzt1e@users.noreply.github.com>
2024-11-22 10:42:47 -0500

    Update khanacademy_revealer.user.js
    I was confused as to why this code wouldn't inject into chrome, and I worked on some things before realizing I needed to turn on Developer Mode. Changes:
    - Improved error handling
    - Cleaned up log logic
    - Easier debugging
    This code got me through highschool, and I'll be going to OSU for Aerospace Engineering in fall of 2025. Thank you, Alex Dubov.

--- khanacademy_revealer.user.js
+++ khanacademy_revealer.user.js
@@ -18,37 +18,36 @@
         }
 
         get isMultiChoice() {
-            return this.type == "multiple_choice";
+            return this.type === "multiple_choice";
         }
 
         get isFreeResponse() {
-            return this.type == "free_response";
+            return this.type === "free_response";
         }
 
         get isExpression() {
-            return this.type == "expression";
+            return this.type === "expression";
         }
 
         get isDropdown() {
-            return this.type == "dropdown";
+            return this.type === "dropdown";
         }
 
         log() {
-            const answer = this.body;
+            const answer = this.body || [];
             const style = "color: coral; -webkit-text-stroke: .5px black; font-size:24px; font-weight:bold;";
-
-            answer.map(ans => {
-                if (typeof ans == "string") {
+            const sanitizedAnswers = answer.map(ans => {
+                if (typeof ans === "string") {
                     if (ans.includes("web+graphie")) {
-                        this.body[this.body.indexOf(ans)] = "";
                         this.printImage(ans);
-                    } else {
-                        answer[answer.indexOf(ans)] = ans.replaceAll("$", "");
+                        return "";
                     }
+                    return ans.replaceAll("$", "");
                 }
+                return ans;
             });
 
-            const text = answer.join("\n");
+            const text = sanitizedAnswers.filter(Boolean).join("\n");
             if (text) {
                 console.log(`%c${text.trim()} `, style);
             }
@@ -57,15 +56,14 @@
         printImage(ans) {
             const url = ans.replace("![](web+graphie", "https").replace(")", ".svg");
             const image = new Image();
-
             image.src = url;
             image.onload = () => {
                 const imageStyle = [
-                    'font-size: 1px;',
-                    'line-height: ', this.height % 2, 'px;',
-                    'padding: ', this.height * .5, 'px ', this.width * .5, 'px;',
-                    'background-size: ', this.width, 'px ', this.height, 'px;',
-                    'background: url(', url, ');'
+                    `font-size: 1px;`,
+                    `line-height: ${image.height}px;`,
+                    `padding: ${image.height / 2}px ${image.width / 2}px;`,
+                    `background-size: ${image.width}px ${image.height}px;`,
+                    `background: url(${url});`
                 ].join(' ');
                 console.log('%c ', imageStyle);
             };
@@ -77,38 +75,45 @@
         return originalFetch.apply(this, arguments).then(async (res) => {
             if (res.url.includes("/getAssessmentItem")) {
                 const clone = res.clone();
-                const json = await clone.json()
+                const json = await clone.json();
 
-let item, question;
+                let item, question;
 
-                    try {
-                        item = json.data.assessmentItem.item.itemData;
-                        question = JSON.parse(item).question;
-                    } catch {
-                        let errorIteration = () => { return localStorage.getItem("error_iter") || 0; }
-                        localStorage.setItem("error_iter", errorIteration() + 1);
+                try {
+                    item = json.data.assessmentItem.item.itemData;
+                    question = JSON.parse(item).question;
+                } catch (error) {
+                    const errorIteration = parseInt(localStorage.getItem("error_iter") || "0", 10);
+                    localStorage.setItem("error_iter", errorIteration + 1);
 
-                        if (errorIteration() < 4) {
-                            return location.reload();
-                        } else {
-                            return console.log("%c An error occurred", "color: red; font-weight: bolder; font-size: 20px;");
-                        }
+                    if (errorIteration < 4) {
+                        location.reload();
+                    } else {
+                        console.error("%cAn error occurred: " + error.message, "color: red; font-weight: bolder; font-size: 20px;");
                     }
+                    return;
+                }
 
-                    if (!question) return;
+                if (!question) return;
 
-                    Object.keys(question.widgets).map(widgetName => {
-                        switch (widgetName.split(" ")[0]) {
-                            case "numeric-input":
-                                return freeResponseAnswerFrom(question).log();
-                            case "radio":
-                                return multipleChoiceAnswerFrom(question).log();
-                            case "expression":
-                                return expressionAnswerFrom(question).log();
-                            case "dropdown":
-                                return dropdownAnswerFrom(question).log();
-                        }
-                    });
+                Object.keys(question.widgets).forEach(widgetName => {
+                    const widget = question.widgets[widgetName];
+                    switch (widgetName.split(" ")[0]) {
+                        case "numeric-input":
+                        case "input-number":
+                            freeResponseAnswerFrom(widget).log();
+                            break;
+                        case "radio":
+                            multipleChoiceAnswerFrom(widget).log();
+                            break;
+                        case "expression":
+                            expressionAnswerFrom(widget).log();
+                            break;
+                        case "dropdown":
+                            dropdownAnswerFrom(widget).log();
+                            break;
+                    }
+                });
             }
 
             if (!window.loaded) {
@@ -119,62 +124,34 @@ let item, question;
             }
 
             return res;
-        })
-    }
+        });
+    };
 
-    function freeResponseAnswerFrom(question) {
-        const answer = Object.values(question.widgets).map((widget) => {
-            if (widget.options?.answers) {
-                return widget.options.answers.map(answer => {
-                    if (answer.status == "correct") {
-                        return answer.value;
-                    }
-                });
-            }
-        }).flat().filter((val) => { return val !== undefined; });
-
+    function freeResponseAnswerFrom(widget) {
+        const answer = (widget.options?.answers || [])
+            .filter(answer => answer.status === "correct")
+            .map(answer => answer.value);
         return new Answer(answer, "free_response");
     }
 
-    function multipleChoiceAnswerFrom(question) {
-        const answer = Object.values(question.widgets).map((widget) => {
-            if (widget.options?.choices) {
-                return widget.options.choices.map(choice => {
-                    if (choice.correct) {
-                        return choice.content;
-                    }
-                });
-            }
-        }).flat().filter((val) => { return val !== undefined; });
-
+    function multipleChoiceAnswerFrom(widget) {
+        const answer = (widget.options?.choices || [])
+            .filter(choice => choice.correct)
+            .map(choice => choice.content);
         return new Answer(answer, "multiple_choice");
     }
 
-    function expressionAnswerFrom(question) {
-        const answer = Object.values(question.widgets).map((widget) => {
-            if (widget.options?.answerForms) {
-                return widget.options.answerForms.map(answer => {
-                    if (Object.values(answer).includes("correct")) {
-                        return answer.value;
-                    }
-                });
-            }
-        }).flat();
-
+    function expressionAnswerFrom(widget) {
+        const answer = (widget.options?.answerForms || [])
+            .filter(answerForm => answerForm.status === "correct")
+            .map(answerForm => answerForm.value);
         return new Answer(answer, "expression");
     }
 
-    function dropdownAnswerFrom(question) {
-        const answer = Object.values(question.widgets).map((widget) => {
-            if (widget.options?.choices) {
-                return widget.options.choices.map(choice => {
-                    if (choice.correct) {
-                        return choice.content;
-                    }
-                });
-            }
-        }).flat();
-
+    function dropdownAnswerFrom(widget) {
+        const answer = (widget.options?.choices || [])
+            .filter(choice => choice.correct)
+            .map(choice => choice.content);
         return new Answer(answer, "dropdown");
     }
 })();
